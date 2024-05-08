857b8e5f0039ee306ebe26a7ab5ea79782ac639a
Jakov Valtsifer <j.valcifer@gmail.com>
2024-05-08 13:46:00 +0300

    protseduuri kutse

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -358,7 +358,7 @@ as begin
 	return (select Name from Employees where Id = @Id)
 end
 
--- ?
+-- protseduuri kutse
 declare @EmployeeName nvarchar(50)
 exec @EmployeeName = spGetNameById2 1
 print 'Name of the employee = ' + @EmployeeName
