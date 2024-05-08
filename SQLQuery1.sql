078aad47f21d440cad5239bad9c3d9a63ded74be
Jakov Valtsifer <j.valcifer@gmail.com>
2024-05-08 12:54:49 +0300

    muudab rida Age täisarvudeni ja pärast sorteerib tabelis Person kirjed otse veeru Age järgi.

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -146,7 +146,7 @@ select top 3 Age, Name from Person
 
 --- näitab esimesed 50% tabelis
 select top 50 percent * from Person
---?
+--muudab rida Age täisarvudeni ja pärast sorteerib tabelis Person kirjed otse veeru Age järgi.
 select * from Person order by cast(Age as int)
 select * from Person order by Age
 
